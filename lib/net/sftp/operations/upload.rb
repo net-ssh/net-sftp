83658ab9020f6e880ae63752f6cd93f833a9484f
Jamis Buck <jamis@37signals.com>
2007-08-06 02:33:59 +0000

    Combine the upload_tree and upload approaches into a single class
    git-svn-id: http://svn.jamisbuck.org/net-sftp/branches/v2@46 69336d94-3fed-0310-b065-b4ee08446d10

--- lib/net/sftp/operations/upload.rb
+++ lib/net/sftp/operations/upload.rb
@@ -16,96 +16,169 @@ module Net; module SFTP; module Operations
       @remote = remote
       @progress = progress || options[:progress]
       @options = options
+      @active = 0
 
       self.logger = base.logger
 
-      @file = local.respond_to?(:read) ? local : File.open(local)
-      @size = @file.respond_to?(:size) ? @file.size : @file.stat.size
+      @uploads = []
 
-      debug { "opening #{remote} for writing" }
-      base.open(remote, "w", &method(:on_open))
+      if recursive?
+        raise "expected a directory to upload" unless File.directory?(local)
+        @stack = [entries_for(local)]
+        @local_cwd = local
+        @remote_cwd = remote
+
+        base.mkdir(remote) do
+          (options[:requests] || RECURSIVE_READERS).to_i.times do
+            break unless process_next_entry
+          end
+        end
+      else
+        raise "expected a file to upload" unless local.respond_to?(:read) || File.file?(local)
+        @stack = [[local]]
+        process_next_entry
+      end
     end
 
-    def preserve?
-      options[:preserve]
+    def recursive?
+      options[:recursive]
     end
 
     private
 
-      REQUESTS  = 4
-      READ_SIZE = 16 * 1024
-
       attr_reader :base
-      attr_reader :handle
-      attr_reader :file
-      attr_reader :offset
       attr_reader :progress
-      attr_reader :active
 
-      def attributes
-        return {} unless preserve? && file.respond_to?(:stat)
+      LiveFile = Struct.new(:lpath, :rpath, :io, :size, :handle)
 
-        { :permissions => file.stat.mode,
-          :atime       => file.stat.atime.to_i,
-          :ctime       => file.stat.ctime.to_i,
-          :mtime       => file.stat.mtime.to_i }
-      end
+      DEFAULT_READ_SIZE   = 32_000
+      SINGLE_FILE_READERS = 2
+      RECURSIVE_READERS   = 16
 
-      def on_open(response)
-        raise "could not upload file: #{response}" unless response.ok?
-        debug { "open #{remote} succeeded" }
-        @handle = response[:handle]
-        @offset = @active = 0
-        update_progress(:start_transfer, self)
-        (options[:requests] || REQUESTS).times { send_next_reader_request }
-      end
+      def process_next_entry
+        if @stack.empty?
+          if @uploads.any?
+            write_next_chunk(@uploads.first)
+          elsif @active.zero?
+            update_progress(:finish)
+          end
+          return false
+        elsif @stack.last.empty?
+          @stack.pop
+          @local_cwd = File.dirname(@local_cwd)
+          @remote_cwd = File.dirname(@remote_cwd)
+          process_next_entry
+        elsif recursive?
+          entry = @stack.last.shift
+          lpath = File.join(@local_cwd, entry)
+          rpath = File.join(@remote_cwd, entry)
 
-      def on_write(response)
-        raise "could not write chunk: #{status}" unless response.ok?
-        @active -= 1
-        update_progress(:update_transfer, self, response.request[:offset])
-        send_next_reader_request
-      end
+          if File.directory?(lpath)
+            @stack.push(entries_for(lpath))
+            @local_cwd = lpath
+            @remote_cwd = rpath
 
-      def on_setstat(response)
-        raise "could not preserve file attributes: #{response}" unless response.ok?
-        finish
-      end
-
-      def on_close(response)
-        raise "could not close remote file: #{response}" unless response.ok?
-        if preserve?
-          base.setstat(remote, attributes, &method(:on_setstat))
-        else
-          finish
-        end
-      end
-
-      def finish
-        file.close
-        update_progress(:finish_transfer, self)
-        debug { "done uploading from #{local} to #{remote}" }
-      end
-
-      def send_next_reader_request
-        if offset >= size
-          if active <= 0
-            base.close(handle, &method(:on_close))
+            @active += 1
+            update_progress(:mkdir, rpath)
+            request = base.mkdir(rpath, &method(:on_mkdir))
+            request[:dir] = rpath
+          else
+            @active += 1
+            request = base.open(rpath, "w", &method(:on_open))
+            request[:file] = LiveFile.new(lpath, rpath)
+            update_progress(:open, request[:file])
           end
         else
           @active += 1
-          data = file.read(options[:read_size] || READ_SIZE)
-          debug { "writing #{data.length} at #{offset}" }
-          request = base.write(handle, offset, data, &method(:on_write))
-          request[:offset] = (@offset += data.length)
+          item = @stack.pop.first
+
+          if item.respond_to?(:read)
+            file = item
+            name = options[:name] || "<memory>"
+          else
+            file = File.open(item)
+            name = item
+          end
+
+          request = base.open(remote, "w", &method(:on_open))
+          request[:file] = LiveFile.new(name, remote, file)
+          update_progress(:open, request[:file])
+        end
+        return true
+      end
+
+      def on_mkdir(response)
+        @active -= 1
+        process_next_entry
+      end
+
+      def on_open(response)
+        @active -= 1
+        file = response.request[:file]
+        raise "open #{file.rpath}: #{response}" unless response.ok?
+
+        file.io ||= File.open(file.lpath)
+        file.handle = response[:handle]
+
+        if file.io.respond_to?(:stat)
+          file.size = file.io.stat.size
+        else
+          file.size = file.io.size
+        end
+
+        @uploads << file
+        write_next_chunk(file)
+
+        if !recursive?
+          (options[:requests] || SINGLE_FILE_READERS).to_i.times { write_next_chunk(file) }
         end
       end
 
+      def on_write(response)
+        @active -= 1
+        file = response.request[:file]
+        raise "write #{file.rpath}: #{response}" unless response.ok?
+        write_next_chunk(file)
+      end
+
+      def on_close(response)
+        @active -= 1
+        file = response.request[:file]
+        raise "close #{file.rpath}: #{response}" unless response.ok?
+        process_next_entry
+      end
+
+      def write_next_chunk(file)
+        if file.io.nil?
+          process_next_entry
+        else
+          @active += 1
+          offset = file.io.pos
+          data = file.io.read(options[:read_size] || DEFAULT_READ_SIZE)
+          if data.nil?
+            update_progress(:close, file)
+            request = base.close(file.handle, &method(:on_close))
+            file.io.close
+            file.io = nil
+            @uploads.delete(file)
+          else
+            update_progress(:write, file, offset, data)
+            request = base.write(file.handle, offset, data, &method(:on_write))
+            request[:file] = file
+          end
+        end
+      end
+
+      def entries_for(local)
+        Dir.entries(local).reject { |v| %w(. ..).include?(v) }
+      end
+
       def update_progress(hook, *args)
-        if progress.respond_to?(hook)
-          progress.send(hook, *args)
+        on = :"on_#{hook}"
+        if progress.respond_to?(on)
+          progress.send(on, self, *args)
         elsif progress.respond_to?(:call)
-          progress.call(hook, *args)
+          progress.call(hook, self, *args)
         end
       end
   end
