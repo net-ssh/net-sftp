e10218421cfca013a63eb89a568bf55d2b839bed
Jamis Buck <jamis@37signals.com>
2008-01-03 05:14:36 +0000

    a few more tests
    git-svn-id: http://svn.jamisbuck.org/net-sftp/branches/v2@59 69336d94-3fed-0310-b065-b4ee08446d10

--- test/base_test.rb
+++ test/base_test.rb
@@ -49,7 +49,44 @@ class BaseTest < Net::SFTP::TestCase
     assert_successful_open("/path/to/file", "r", :permissions => 0765)
   end
 
+  def test_v4_open_with_permissions_should_specify_permissions
+    expect_open("/path/to/file", "r", 0765, :server_version => 4)
+    assert_successful_open("/path/to/file", "r", :permissions => 0765)
+  end
+
+  def test_v5_open_read_only_shuld_invoke_callback
+    expect_open("/path/to/file", "r", 0765, :server_version => 5)
+    assert_successful_open("/path/to/file", "r", :permissions => 0765)
+  end
+
+  def test_v6_open_with_permissions_should_specify_permissions
+    expect_open("/path/to/file", "r", 0765, :server_version => 6)
+    assert_successful_open("/path/to/file", "r", :permissions => 0765)
+  end
+
+  def test_close_should_send_close_request_and_invoke_callback
+    expect_sftp_session do |channel|
+      channel.sends_packet(FXP_CLOSE, :long, 0, :string, "handle")
+      channel.gets_packet(FXP_STATUS, :long, 0, :long, 0)
+    end
+
+    called = false
+    assert_scripted_command do
+      sftp.base.close("handle") do |response|
+        called = true
+        assert response.ok?
+      end
+    end
+    assert called
+  end
+
   private
+
+    class V1 < Net::SFTP::Protocol::V01::Base
+    end
+
+    class V5 < Net::SFTP::Protocol::V05::Base
+    end
 
     def assert_scripted_command
       assert_scripted do
@@ -72,23 +109,41 @@ class BaseTest < Net::SFTP::TestCase
     end
 
     def expect_open(path, mode, perms, options={})
-      fail = options.delete(:fail)
-      flags = case mode
-        when "r"  then Net::SFTP::Protocol::V01::Base::F_READ
-        when "w"  then Net::SFTP::Protocol::V01::Base::F_WRITE | Net::SFTP::Protocol::V01::Base::F_TRUNC | Net::SFTP::Protocol::V01::Base::F_CREAT
-        when "rw" then Net::SFTP::Protocol::V01::Base::F_WRITE | Net::SFTP::Protocol::V01::Base::F_READ
-        when "a"  then Net::SFTP::Protocol::V01::Base::F_APPEND | Net::SFTP::Protocol::V01::Base::F_CREAT
-        else raise ArgumentError, "unsupported mode #{mode.inspect}"
-      end
+      version = options[:server_version] || 6
 
-      if perms
-        attrs = [:long, 0x4, :long, perms]
-      else
-        attrs = [:long, 0]
-      end
+      fail = options.delete(:fail)
+
+      attrs = [:long, perms ? 0x4 : 0]
+      attrs += [:byte, 1] if version >= 4
+      attrs += [:long, perms] if perms
 
       expect_sftp_session(options) do |channel|
-        channel.sends_packet(FXP_OPEN, :long, 0, :string, path, :long, flags, *attrs)
+        if version >= 5
+          flags, access = case mode
+            when "r" then 
+              [V5::F_OPEN_EXISTING, V5::ACE::F_READ_DATA | V5::ACE::F_READ_ATTRIBUTES]
+            when "w" then
+              [V5::F_CREATE_TRUNCATE, V5::ACE::F_WRITE_DATA | V5::ACE::F_WRITE_ATTRIBUTES]
+            when "rw" then
+              [V5::F_OPEN_OR_CREATE, V5::ACE::F_READ_DATA | V5::ACE::F_READ_ATTRIBUTES | V5::ACE::F_WRITE_DATA | V5::ACE::F_WRITE_ATTRIBUTES]
+            when "a" then
+              [V5::F_OPEN_OR_CREATE | V5::F_APPEND_DATA, V5::ACE::F_WRITE_DATA | V5::ACE::F_WRITE_ATTRIBUTES | V5::ACE::F_APPEND_DATA]
+            else raise ArgumentError, "unsupported mode #{mode.inspect}"
+          end
+
+          channel.sends_packet(FXP_OPEN, :long, 0, :string, path, :long, access, :long, flags, *attrs)
+        else
+          flags = case mode
+            when "r"  then V1::F_READ
+            when "w"  then V1::F_WRITE | V1::F_TRUNC | V1::F_CREAT
+            when "rw" then V1::F_WRITE | V1::F_READ
+            when "a"  then V1::F_APPEND | V1::F_CREAT
+            else raise ArgumentError, "unsupported mode #{mode.inspect}"
+          end
+
+          channel.sends_packet(FXP_OPEN, :long, 0, :string, path, :long, flags, *attrs)
+        end
+
         if fail
           channel.gets_packet(FXP_STATUS, :long, 0, :long, fail)
         else
