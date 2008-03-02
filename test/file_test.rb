9f80e850efb8db5b6a888ab93585f63cdb49699b
Jamis Buck <jamis@37signals.com>
2008-03-02 08:54:47 -0700

    starting to flesh out the file tests

--- test/file_test.rb
+++ test/file_test.rb
@@ -1,9 +1,32 @@
 require 'common'
 
-class FileTest < Net::SFTP::TestCase
-  # pos= should set pos
-  # pos= should reset eof
-  # close should close file and set handle to nil
+class FileOperationsTest < Net::SFTP::TestCase
+  def setup
+    @sftp = mock("sftp")
+    @file = Net::SFTP::Operations::File.new(@sftp, "handle")
+  end
+
+  def test_pos_assignment_should_set_position
+    @file.pos = 15
+    assert_equal 15, @file.pos
+  end
+
+  def test_pos_assignment_should_reset_eof
+    @sftp.expects(:read!).with("handle", 0, 8192).returns(nil)
+    assert !@file.eof?
+    @file.read
+    assert @file.eof?
+    @file.pos = 0
+    assert !@file.eof?
+  end
+
+  def test_close_should_close_handle_and_set_handle_to_nil
+    assert_equal "handle", @file.handle
+    @sftp.expects(:close!).with("handle")
+    @file.close
+    assert_nil @file.handle
+  end
+
   # eof? should be false if not at eof
   # eof? should be false if at eof but data in buffer
   # eof? should be true i f at eof and no data in buffer
@@ -25,4 +48,8 @@ class FileTest < Net::SFTP::TestCase
   # puts should print each argument and append '\n'
   # puts should not append newline if argument ends in newline
   # stat should return attributes object for handle
+
+  private
+
+    
 end
