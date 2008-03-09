66989c0ea5ca8510a5bdbf668dd20edbfe18ffda
Jamis Buck <jamis@37signals.com>
2008-03-08 23:22:38 -0700

    tests and docs for the abstract Protocol::Base superclass

--- lib/net/sftp/protocol/base.rb
+++ lib/net/sftp/protocol/base.rb
@@ -3,18 +3,26 @@ require 'net/sftp/constants'
 
 module Net; module SFTP; module Protocol
 
+  # The abstract superclass of the specific implementations for each supported
+  # SFTP protocol version. It implements general packet parsing logic, and
+  # provides a way for subclasses to send requests.
   class Base
     include Net::SSH::Loggable
     include Net::SFTP::Constants
 
+    # The SFTP session object that acts as client to this protocol instance
     attr_reader :session
 
+    # Create a new instance of a protocol driver, servicing the given session.
     def initialize(session)
       @session = session
       self.logger = session.logger
       @request_id_counter = -1
     end
 
+    # Attept to parse the given packet. If the packet is of an unsupported
+    # type, an exception will be raised. Returns the parsed data as a hash
+    # (the keys in the hash are packet-type specific).
     def parse(packet)
       case packet.type
       when FXP_STATUS then parse_status_packet(packet)
@@ -28,6 +36,9 @@ module Net; module SFTP; module Protocol
 
     private
 
+      # Send a new packet of the given type, and with the given data arguments.
+      # A new request identifier will be allocated to this request, and will
+      # be returned.
       def send_request(type, *args)
         @request_id_counter += 1
         session.send_packet(type, :long, @request_id_counter, *args)
