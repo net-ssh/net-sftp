8f1eb74d949280c9bb442380faba69523721c8ea
Jamis Buck <jamis@37signals.com>
2008-05-01 22:08:18 -0600

    prepping for the 2.0 release

--- lib/net/sftp/version.rb
+++ lib/net/sftp/version.rb
@@ -4,9 +4,9 @@ module Net; module SFTP
 
   # Describes the current version of the Net::SFTP library.
   class Version < Net::SSH::Version
-    MAJOR = 1
-    MINOR = 99
-    TINY  = 1
+    MAJOR = 2
+    MINOR = 0
+    TINY  = 0
 
     # The current version, as a Version instance
     CURRENT = new(MAJOR, MINOR, TINY)
