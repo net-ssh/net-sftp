84847dbb4361e9d35f064974bd292f37c3e0f6d1
Jamis Buck <jamis@37signals.com>
2008-05-29 15:39:55 -0600

    version bump

--- lib/net/sftp/version.rb
+++ lib/net/sftp/version.rb
@@ -6,7 +6,7 @@ module Net; module SFTP
   class Version < Net::SSH::Version
     MAJOR = 2
     MINOR = 0
-    TINY  = 0
+    TINY  = 1
 
     # The current version, as a Version instance
     CURRENT = new(MAJOR, MINOR, TINY)
