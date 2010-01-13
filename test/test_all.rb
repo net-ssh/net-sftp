64b5b6076d17e82b3888f2a896a35aad2ff069c6
delano <delano@solutious.com>
2010-01-13 18:21:43 -0500

    require common in test/test_all.rb. Why does it override LOAD_PATH otherwise?

--- test/test_all.rb
+++ test/test_all.rb
@@ -1,4 +1,7 @@
-#  $ ruby -Ilib -Itest -rrubygems test/test_all.rb
+#  $ ruby -I../net-ssh/lib -Ilib -Itest -rrubygems test/test_all.rb
+#require 'net/ssh'
+#puts Net::SSH::Version::CURRENT
+require 'common'
 Dir.chdir(File.dirname(__FILE__)) do
   Dir['**/test_*.rb'].each { |file| require(file) }
 end
