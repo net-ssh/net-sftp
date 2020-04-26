9f004f6f464bc047f7b31e3954008d3538ad11c0
Thibaut Barrère <thibaut.barrere@gmail.com>
2020-04-26 16:03:34 +0200

    Add test to cover protocol version passing

--- test/test_start.rb
+++ test/test_start.rb
@@ -9,11 +9,26 @@ class StartTest < Net::SFTP::TestCase
     sftp = mock('sftp')
     # TODO: figure out how to verify a block is passed, and call it later.
     # I suspect this is hard to do properly with mocha.
-    Net::SFTP::Session.expects(:new).with(ssh).returns(sftp)
+    Net::SFTP::Session.expects(:new).with(ssh, nil).returns(sftp)
     sftp.expects(:connect!).returns(sftp)
     sftp.expects(:loop)
     
     Net::SFTP.start('host', 'user') do
+      # NOTE: currently not called!
+    end
+  end
+  
+  def test_with_block_and_options
+    ssh = mock('ssh')
+    ssh.expects(:close)
+    Net::SSH.expects(:start).with('host', 'user', auth_methods: ["password"]).returns(ssh)
+
+    sftp = mock('sftp')
+    Net::SFTP::Session.expects(:new).with(ssh, 3).returns(sftp)
+    sftp.expects(:connect!).returns(sftp)
+    sftp.expects(:loop)
+    
+    Net::SFTP.start('host', 'user', {auth_methods: ["password"]}, {version: 3}) do
       # NOTE: currently not called!
     end
   end
